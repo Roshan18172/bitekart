8c4dce12227ecb5c57744258b436411c5684cd40
Roshan Pal <roshanpal18172@gmail.com>
2026-07-02 20:08:19 +0530

    update db url

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const dbURI = 'mongodb://localhost:27017/bitekart?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
+const dbURI = 'mongodb://roshanpal18172:rpal%40123@ac-rr94adj-shard-00-00.jx2f8hv.mongodb.net:27017,ac-rr94adj-shard-00-01.jx2f8hv.mongodb.net:27017,ac-rr94adj-shard-00-02.jx2f8hv.mongodb.net:27017/bitekart?ssl=true&replicaSet=atlas-bre33u-shard-0&authSource=admin&retryWrites=true&w=majority';
 
 const connectDB = async () => {
   try {
